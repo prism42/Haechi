879c51220731544ecb89b57d398acdc0b5d9c8ff
tanzhenzhong <tanzhenzhong>
2020-01-08 11:14:34 +0800

    Temporary preservation of project

--- app/src/main/java/com/prism/haechi/module/base/BaseActivity.java
+++ app/src/main/java/com/prism/haechi/module/base/BaseActivity.java
@@ -2,6 +2,8 @@ package com.prism.haechi.module.base;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,5 +19,24 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mContext = this;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                //Monitor screen press action
+                break;
+            case MotionEvent.ACTION_UP:
+                //Monitor screen press lift action
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
